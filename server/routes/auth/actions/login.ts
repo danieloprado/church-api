4b0095eb7d023944ae0db52c49d0ba09e3913388
Daniel Prado <danielprado.ad@gmail.com>
2016-10-04 12:39:57 -0300

    return

--- server/routes/auth/actions/login.ts
+++ server/routes/auth/actions/login.ts
@@ -1,9 +1,13 @@
 const authService = require('services/auth');
+import * as express from 'express';
 
-module.exports = function(req, res, next) {
-  authService.login(req.body.email, req.body.password).then(token => {
+
+export async function login(req: express.Request, res: express.Response, next: express.NextFunction): Promise<any> {
+  try {
+    var token = await authService.login(req.body.email, req.body.password);
     res.json(token);
-  }).catch(err => {
+  } catch (err) {
+
     switch (err.message) {
       case 'user-not-found':
       case 'invalid-password':
@@ -11,5 +15,6 @@ module.exports = function(req, res, next) {
       default:
         return next(err);
     }
-  });
+
+  }
 };
